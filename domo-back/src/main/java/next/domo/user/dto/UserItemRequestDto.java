81f94ef14808e267499fc2d4e0571f55b33e8a75
jjjh02 <jinnyshur@korea.ac.kr>
2025-05-04 10:26:42 +0900

    Feat: item 관련 기능 추가

---
+++ domo-back/src/main/java/next/domo/user/dto/UserItemRequestDto.java
@@ -0,0 +1,8 @@
+package next.domo.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserItemRequestDto {
+    private Long itemId;
+}
---
+++ domo-back/src/main/java/next/domo/user/service/UserItemService.java
@@ -0,0 +1,48 @@
+package next.domo.user.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import next.domo.file.entity.Item;
+import next.domo.file.repository.ItemRepository;
+import next.domo.user.dto.UserItemRequestDto;
+import next.domo.user.dto.UserItemStoreResponseDto;
+import next.domo.user.entity.User;
+import next.domo.user.entity.UserItem;
+import next.domo.user.repository.UserItemRepository;
+import next.domo.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UserItemService {
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
+    private final UserItemRepository userItemRepository;
+
+    @Transactional
+    public void addItemToUser(Long userId, UserItemRequestDto userItemRequestDto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Item item = itemRepository.findById(userItemRequestDto.getItemId())
+                .orElseThrow(() -> new IllegalArgumentException("Item not found"));
+
+        UserItem userItem = UserItem.builder()
+                .user(user)
+                .item(item)
+                .build();
+
+        userItemRepository.save(userItem);
+    }
+
+    public List<UserItemStoreResponseDto> getStoreItemsByUser(Long userId) {
+        List<Item> allItems = itemRepository.findAll();
+        List<Long> ownedItemIds = userItemRepository.findItemIdsByUserId(userId);
+
+        return allItems.stream()
+                .map(item -> UserItemStoreResponseDto.from(item, ownedItemIds.contains(item.getItemId())))
+                .toList();
+    }
+}
