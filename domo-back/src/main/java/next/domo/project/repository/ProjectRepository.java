6b6207f44167ee488e55cb81dfcfe354e6042151
KangMinSun <chim38713@gmail.com>
2025-05-16 08:48:39 +0900

    [Mod] 가장 최근 접속한 미완료 프로젝트 조회 기능으로 수정

--- domo-back/src/main/java/next/domo/project/repository/ProjectRepository.java
+++ domo-back/src/main/java/next/domo/project/repository/ProjectRepository.java
@@ -3,6 +3,8 @@ package next.domo.project.repository;
 import next.domo.project.entity.Project;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,9 @@ import java.util.Optional;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     boolean existsByProjectTag_ProjectTagId(Long projectTagId);
     List<Project> findByUserUserId(Long userId);
-    Optional<Project> findTopByUserUserIdOrderByLastAccessedAtDesc(Long userId);
     List<Project> findByProjectTag_ProjectTagIdIn(List<Long> tagIds, Sort sort);
     List<Project> findAll(Sort sort);
+
+    @Query("SELECT p FROM Project p WHERE p.user.userId = :userId AND p.projectStatus <> 'DONE' ORDER BY p.lastAccessedAt DESC")
+    Optional<Project> findTopByUserIdAndNotDoneOrderByLastAccessedAtDesc(@Param("userId") Long userId);
 }
