b54bd17fa37bb5239aff870a6df49b766836b1a4
jjjh02 <jinnyshur@korea.ac.kr>
2025-05-15 13:34:06 +0900

    Mod: delete cascade 설정 추가

--- domo-back/src/main/java/next/domo/project/entity/ProjectTag.java
+++ domo-back/src/main/java/next/domo/project/entity/ProjectTag.java
@@ -3,6 +3,8 @@ package next.domo.project.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import next.domo.user.entity.User;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "project_tag")
@@ -20,5 +22,6 @@ public class ProjectTag {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 }
